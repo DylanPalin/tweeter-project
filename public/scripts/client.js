67a53b4f29e2addaad3c9062a7e992ae50c410ad
Dylan Palin <dylanpalin@gmail.com>
2023-12-08 23:35:39 -0700

    forgot to push commits, tweets loading w/o refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,98 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+// create tweet element from tweet data
+function createTweetElement(tweet) {
+  const { user, content, createdAt } = tweet;
+  const date = timeago.format(tweet.createdAt);
+  const $tweet = `
+      <article>
+          <header class="userThumbs">
+            <img class="pfpThumb" src="${user.avatars}"></img>
+            <h3>${user.name}</h3>
+            <h4>${user.handle}</h4>
+          </header>
+
+          <div class="tweetContent">
+            <p class="userTweet">${content.text}</p>
+          </div>
+
+          <footer class="navIcons">
+            <p class="daysOld">${date}</div>
+            <div class="icons">
+              <button class="flagButton">
+                <i class="fa-solid fa-flag"></i>
+                <button class="retweetButton">
+                  <i class="fa-solid fa-retweet"></i>
+                  <button class="likeButton">
+                    <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+    `;
+
+  return $tweet;
+}
+
+// loop through tweets and call above function to render them on index
+const renderTweets = tweets => {
+  const existingTweets = $('.tweet-container');
+  for (const tweet of tweets) {
+    existingTweets.prepend(createTweetElement(tweet));
+  }
+};
+
+$(document).ready(function (event) {
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (tweets) {
+        renderTweets(tweets);
+      })
+    };
+  loadTweets();    
+
+
+
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+
+    let formData = {
+      text: $('#textarea').val()
+    };
+
+    if (formData.text.length > 140) {
+      $('#textarea').addClass('error');
+      $('#error').remove();
+      $('#tweetButton').after('<p id="error">Your tweet is too long!</p>');
+    }
+
+    if (formData.text.length === 0) {
+      $('#textarea').addClass('error');
+      $('#error').remove();
+      $('#tweetButton').after('<p id="error">Your tweet is too short!</p>');
+    }
+
+    if (formData.text.length > 0 && formData.text.length <= 140) {
+      $('#error').remove();
+      $('#textarea').removeClass('error');
+
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: formData,
+      })
+        .done(() => {
+
+          $.get('/tweets', function (theData) {
+            $('.tweet-container').prepend(createTweetElement(theData[theData.length - 1]));
+            $('#publish')[0].reset();
+            $('#counter').text(140);
+
+          });
+        })
+      }
+    });
+});
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -7,6 +7,7 @@ const tweetsRoutes  = express.Router();
 
 module.exports = function(DataHelpers) {
 
+  // GET /tweets
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -17,6 +18,7 @@ module.exports = function(DataHelpers) {
     });
   });
 
+  // POST /tweets
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
@@ -28,7 +30,7 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      createDate: Date.now()
     };
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
@@ -38,6 +40,8 @@ module.exports = function(DataHelpers) {
       }
     });
   });
+
+
 
   return tweetsRoutes;
 
